6fd4365529bfc78936569ac849537bb0c866db43
suzushi-tw <ianhuang2023a@gmail.com>
2023-10-14 10:29:33 +0200

    message

---
+++ app/about/page.tsx
@@ -0,0 +1,88 @@
+export default function About() {
+  return (
+    <div className="space-y-12 text-md md:text-lg text-gray-500 pb-4 md:pb-8">
+      <p>
+        受夠了付費工具了嗎?那試試我們的平台吧，完全免費且黨案完全不會存在我們的伺服器
+      </p>
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🖼️ 圖像轉換:
+        </h2>
+        <p>
+        用我們的圖像轉換工具釋放您的創造力。無論您需要調整大小、裁剪、旋轉還是轉換格式，我們都能幫您搞定。從JPEG到PNG，以及其他所有格式，輕鬆地提升您的視覺內容。
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🎵 音檔轉換:
+        </h2>
+        <p>
+        讓您的音頻項目聲音更響亮！利用我們的音頻轉換功能，您可以將音頻文件在不同的格式之間進行轉換，例如MP3、WAV或AAC。調整比特率、剪裁和合併音頻文件，創造您的完美音軌。
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🎥 影片轉換:
+        </h2>
+        <p>
+        燈光、攝影機、動作！無限制地編輯和轉碼視頻。從改變視頻格式到剪切和合併片段，您可以為任何平台或目的創造令人驚嘆的視頻內容。
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🚀 無限次數，無限制使用:
+        </h2>
+        <p>
+        與其他服務不同，我們相信在沒有隱藏費用或限制的情況下賦予您的創造力。您可以根據自己的心願轉換任意數量的圖像、音頻文件和視頻，而不會花費您一分錢。
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🌐 隨時隨地使用:
+        </h2>
+        <p>
+          只要有網路都可以使用
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🔒 安全且私密:
+        </h2>
+        <p>
+          你的檔案都會經過嚴格的保護
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          💡 User-Friendly Interface:
+        </h2>
+        <p>
+          淺顯易懂的UI
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          🌟 It's Free, It's Powerful, It's Modifio:
+        </h2>
+        <p>
+          Experience the freedom to convert images, audio, and video without
+          boundaries. Elevate your multimedia projects with the unlimited
+          potential of Modifio.
+        </p>
+      </div>
+
+      <p>
+        Join the revolution of content creators, professionals, and enthusiasts
+        who are transforming the way they work with multimedia. Get started
+        today and let your creativity shine like never before!
+      </p>
+    </div>
+  );
+}
